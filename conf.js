d28d54991e15bdb4a37034d543f1a88912b54baf
Erik Orozco <erik.orozco@bigcommerce.com>
2019-01-09 23:09:27 -0600

    updating beautiful reporter dependency version

--- conf.js
+++ conf.js
@@ -10,6 +10,16 @@ exports.config = {
 
     onPrepare: function () {
         const SpecReporter = require("jasmine-spec-reporter").SpecReporter;
+        const HtmlReporter = require("protractor-beautiful-reporter");
+
         jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+        jasmine.getEnv().addReporter(new HtmlReporter({
+            baseDirectory: `./reports/Tests Results`,
+            screenshotsSubfolder: "images",
+            jsonsSubfolder: "json",
+            docName: "report.html",
+            docTitle: "Bigcommerce app tests results",
+            takeScreenShotsOnlyForFailedSpecs: true
+        }).getJasmine2Reporter());
     }
   };
